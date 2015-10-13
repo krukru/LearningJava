f86c75b8372f22c1403ec4ec7ec1d3338102a7f9
Marko Kruljac <marko.kruljac@degordian.com>
2015-10-13 23:15:26 +0200

    finised

--- src/kru/ctci/chapter1/Problem6.java
+++ src/kru/ctci/chapter1/Problem6.java
@@ -9,15 +9,35 @@ public class Problem6 {
     if (height != width) {
       throw new IllegalArgumentException("Cannot rotate non square matrix in-place");
     }
-    for (int i = 0; i < height * width / 2; i++) {
-      Tuple<Integer, Integer> point = arrayToMatrixCoordinates(i, width);
-      Tuple<Integer, Integer> mirrorPoint = new Tuple<Integer, Integer>()
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < (height - (2 * i)) - 1; j++) {
+        /* every time we increase i we eliminate two corners
+         * That's why height - (2 * i)
+         * We also don't want to rotate the last element,
+         * since it is already rotated.
+         * */
+        Tuple<Integer, Integer> p1 = Tuple.create(i, i + j);
+        Tuple<Integer, Integer> p2 = getRotatedCoordinates(p1.getItem1(), p1.getItem2(), height);
+        Tuple<Integer, Integer> p3 = getRotatedCoordinates(p2.getItem1(), p2.getItem2(), height);
+        Tuple<Integer, Integer> p4 = getRotatedCoordinates(p3.getItem1(), p3.getItem2(), height);
+        int temp1 = matrix[p1.getItem1()][p1.getItem2()];
+        int temp2 = matrix[p2.getItem1()][p2.getItem2()];
+        int temp3 = matrix[p3.getItem1()][p3.getItem2()];
+        int temp4 = matrix[p4.getItem1()][p4.getItem2()];
+        matrix[p1.getItem1()][p1.getItem2()] = temp4;
+        matrix[p2.getItem1()][p2.getItem2()] = temp1;
+        matrix[p3.getItem1()][p3.getItem2()] = temp2;
+        matrix[p4.getItem1()][p4.getItem2()] = temp3;
+      }
     }
   }
 
-  private Tuple<Integer, Integer> arrayToMatrixCoordinates(int index, int matrixWidth) {
-    int i = index / matrixWidth;
-    int j = index - i;
-    return new Tuple<Integer, Integer>(i, j);
+  private Tuple<Integer, Integer> getRotatedCoordinates(int i, int j, int matrixSize) {
+    return new Tuple<Integer, Integer>(j, matrixSize - 1 - i);
+  }
+
+  private int getRotatedPoint(int[][] matrix, int i, int j) {
+    int matrixSize = matrix.length - 1;
+    return matrix[j][matrixSize - i];
   }
 }

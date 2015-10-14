83e20d3ad6b0178669887d31eef6b2a67d11312f
Marko Kruljac <kruljac.marko@gmail.com>
2015-10-14 19:44:29 +0200

    initial attempt. needs refactoring and a compile.

--- src/kru/ctci/chapter1/Problem7.java
+++ src/kru/ctci/chapter1/Problem7.java
@@ -1,7 +1,34 @@
 package kru.ctci.chapter1;
 
+import kru.collections.ArrayList;
+
+import java.collections.List;
+
 public class Problem7 {
+
+  private static final ERASURE_NUMBER = 0;
+
+  /* To Do: refactor so this methods calls internalReplace
+   * void internalReplace(int[][] matrix, int erasurNumber, int replaceWithNumber);
+   **/
   public void replaceItemsInRowAndColumn(int[][] matrix) {
-    return;
+    List<Tuple<Integer, Integer>> erasurePoints = new ArrayList<>();
+    int height = matrix.length();
+    int width = matrix[0].length();
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (matrix[i][j] == ERASURE_NUMBER) {
+          erasurePoints.add(Tuple.create(i, j)); /* Boxing required? */
+	}
+      }
+    }
+    for (Tuple<Integer, Integer> point : erasurePoints) {
+      for (int i = 0; i < height; i++) {
+        matrix[i][point.getItem2()] = ERASURE_NUMBER;
+      }
+      for (int j = 0; j < width; j++) {
+        matrix[point.getItem1()][j] = ERASURE_NUMBER;
+      }
+    }
   }
 }

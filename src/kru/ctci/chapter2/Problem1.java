fadd96bcfae08b9c2b0ee03440402d4d95b324a4
Marko Kruljac <marko.kruljac@degordian.com>
2015-10-15 21:44:00 +0200

    stash

--- src/kru/ctci/chapter2/Problem1.java
+++ src/kru/ctci/chapter2/Problem1.java
@@ -1,4 +1,21 @@
 package kru.ctci.chapter2;
 
+import java.util.Iterator;
+
+import kru.collections.HashSet;
+import kru.collections.LinkedList;
+
 public class Problem1 {
+  public <E> void removeDuplicates(LinkedList<E> list) {
+    HashSet<E> lookup = new HashSet<>();
+    for (Iterator<E> it = list.iterator(); it.hasNext(); it.next()) {
+      E element = it.next();
+      if (lookup.contains(element)) {
+        it.remove();
+      }
+      else {
+        lookup.add(element);
+      }
+    }
+  }
 }
--- test/kru/ctci/chapter2/Problem1Test.java
+++ test/kru/ctci/chapter2/Problem1Test.java
@@ -1,7 +1,34 @@
 package kru.ctci.chapter2;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import kru.collections.HashSet;
+import kru.collections.LinkedList;
 
 public class Problem1Test {
 
+  Problem1 p1 = new Problem1();
+  private LinkedList<Integer> list = new LinkedList<>();
+
+  @Test
+  public void basicNonDeterministicTest() {
+    Random rnd = new Random();
+    for (int i = 0; i < 10000; i++) {
+      list.add(rnd.nextInt(50));
+    }
+    p1.removeDuplicates(list);
+    HashSet<Integer> lookup = new HashSet<>();
+    for (int element : list) {
+      if (lookup.contains(element)) {
+        fail("Duplicate found");
+      }
+      else {
+        lookup.add(element);
+      }
+    }
+  }
 }

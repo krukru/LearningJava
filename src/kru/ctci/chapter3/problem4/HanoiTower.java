6729b91dbbd69f339f8aa1253fff1bac80263b74
Kru <kruljac.marko@gmail.com>
2015-10-17 02:33:47 +0200

    complied to the task constraint that it must be solved using a stack

--- src/kru/ctci/chapter3/problem4/HanoiTower.java
+++ src/kru/ctci/chapter3/problem4/HanoiTower.java
@@ -1,6 +1,8 @@
 package kru.ctci.chapter3.problem4;
 
-public class HanoiTower {
+import kru.collections.Stack;
+
+public class HanoiTower extends Stack<HanoiDisc> {
   private final char label;
 
   public HanoiTower(char label) {

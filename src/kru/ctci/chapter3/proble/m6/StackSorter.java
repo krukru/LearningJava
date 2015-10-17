32bce4869421f9a32974be243a4c7443f5aa42cf
Kru <kruljac.marko@gmail.com>
2015-10-17 20:40:11 +0200

    finished, but quite possibly not optimal

---
+++ src/kru/ctci/chapter3/proble/m6/StackSorter.java
@@ -0,0 +1,30 @@
+package kru.ctci.chapter3.proble.m6;
+
+import kru.collections.Stack;
+
+public class StackSorter {
+  public <E extends Comparable<E>> void sort(Stack<E> stack) {
+    if (stack.size() > 1) {
+      Stack<E> helperStack = new Stack<>();
+      int stackSize = stack.size();
+      for (int numberOfSortedElements = 0; numberOfSortedElements < stackSize;
+           numberOfSortedElements++) {
+        E minimalElement = stack.peek();
+        while (stack.size() > numberOfSortedElements) {
+          E poppedElement = stack.pop();
+          helperStack.push(poppedElement);
+          if (poppedElement.compareTo(minimalElement) > 0) {
+            minimalElement = poppedElement;
+          }
+        }
+        stack.push(minimalElement);
+        while (helperStack.isEmpty() == false) {
+          E element = helperStack.pop();
+          if (element != minimalElement) {
+            stack.push(element);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ test/kru/ctci/chapter3/proble/m6/StackSorterTest.java
@@ -0,0 +1,30 @@
+package kru.ctci.chapter3.proble.m6;
+
+import kru.collections.Stack;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class StackSorterTest {
+
+  @Test
+  public void testSort2() {
+    Stack<Integer> stack = new Stack<>();
+    Random rnd = new Random();
+    for (int i = 0; i < 100; i++) {
+      stack.add(rnd.nextInt(10000));
+    }
+    StackSorter ss = new StackSorter();
+    ss.sort(stack);
+    int minimalElement = stack.peek();
+    while (stack.isEmpty() == false) {
+      int nextElement = stack.pop();
+      if (nextElement < minimalElement) {
+        fail();
+      }
+      minimalElement = nextElement;
+    }
+  }
+}

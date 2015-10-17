ce656f5a93e510d0e1a3af8d5f1c2b3165a7e685
Kru <kruljac.marko@gmail.com>
2015-10-17 02:18:06 +0200

    added popAtIndex

--- src/kru/ctci/chapter3/problem3/PlateStack.java
+++ src/kru/ctci/chapter3/problem3/PlateStack.java
@@ -1,15 +1,22 @@
 package kru.ctci.chapter3.problem3;
 
-import kru.collections.LinkedList;
+import kru.collections.HashMap;
 import kru.collections.Stack;
 
 public class PlateStack<E> {
 
-  private static final int MAX_SINGLE_STACK_SIZE = 5;
+  private static final int MAX_SINGLE_STACK_SIZE = 10;
 
   private int singleStackSize;
-  private LinkedList<Stack<E>> stackCollection = new LinkedList<>();
+  /*
+   * We need a data structure that will allow us quick insert time (amortized O(1)) and quick
+   * random access (index retrieval). Since every HashMap Entry would be a Stack of it's own, it
+   * is to expect that there won't be too much of resizing to do for the HashMap.
+   */
+  private HashMap<Integer, Stack<E>> stackCollection = new HashMap<>(256); /* Should be enaugh */
 
+  private int currentIndex = 0;
+  private int size = 0;
 
   public PlateStack() {
     this(MAX_SINGLE_STACK_SIZE);
@@ -18,35 +25,52 @@ public class PlateStack<E> {
   public PlateStack(int singleStackSize) {
     this.singleStackSize = singleStackSize;
     Stack<E> initialStack = new Stack<>();
-    stackCollection.add(initialStack);
+    stackCollection.put(currentIndex, initialStack);
   }
 
   public boolean push(E element) {
-    if (stackCollection.getLast().size() == MAX_SINGLE_STACK_SIZE) {
+    if (stackCollection.get(currentIndex).size() == MAX_SINGLE_STACK_SIZE) {
       Stack<E> newStack = new Stack<>();
-      stackCollection.addLast(newStack);
+      currentIndex += 1;
+      stackCollection.put(currentIndex, newStack);
     }
-    return stackCollection.getLast().push(element);
+    size += 1;
+    return stackCollection.get(currentIndex).push(element);
   }
 
   public E pop() {
-    E result = stackCollection.getLast().pop();
-    if (stackCollection.getLast().isEmpty()) {
-      stackCollection.removeLast();
-      if (stackCollection.isEmpty()) {
+    E result = stackCollection.get(currentIndex).pop();
+    if (stackCollection.get(currentIndex).isEmpty()) {
+      stackCollection.remove(currentIndex);
+      currentIndex -= 1;
+      if (currentIndex == -1) {
         /* In case we cleared up the entire stack, initialize it for the next push */
-        stackCollection.addFirst(new Stack<E>());
+        currentIndex = 0;
+        stackCollection.put(currentIndex, new Stack<E>());
       }
     }
+    size -= 1;
     return result;
   }
 
+  public E popAtStack(int index) {
+    if (index < 0 || index > stackCollection.size()) {
+      throw new IndexOutOfBoundsException();
+    }
+    size -= 1;
+    return stackCollection.get(index).pop();
+  }
+
   public E peek() {
-    return stackCollection.getLast().peek();
+    return stackCollection.get(currentIndex).peek();
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
   }
 
   public int size() {
-    return MAX_SINGLE_STACK_SIZE * (stackCollection.size() - 1) + stackCollection.getLast().size();
+    return size;
   }
 
 }
--- test/kru/ctci/chapter3/problem3/PlateStackTest.java
+++ test/kru/ctci/chapter3/problem3/PlateStackTest.java
@@ -20,9 +20,31 @@ public class PlateStackTest {
     assertEquals(98, plateStack.pop().intValue());
     assertEquals(97, plateStack.pop().intValue());
     assertEquals(96, plateStack.peek().intValue());
+    assertEquals(97, plateStack.size());
     for (int i = 96; i >= 0; i--) {
       assertEquals(i, plateStack.pop().intValue());
     }
+    assertEquals(0, plateStack.size());
+    assertTrue(plateStack.isEmpty());
   }
 
+  @Test
+  public void testPopAtStack() {
+    for (int i = 0; i < 100; i++) {
+      plateStack.push(i);
+    }
+    plateStack.popAtStack(0); /* popped 9 */
+    plateStack.popAtStack(1); /* popped 18 */
+    plateStack.popAtStack(2); /* popped 27 */
+    plateStack.popAtStack(3); /* popped 36 */
+    plateStack.popAtStack(4); /* popped 45 */
+    assertEquals(95, plateStack.size());
+    for (int i = 99; i >= 0; i--) {
+      if (i == 9 || i == 18 || i == 27 || i == 36 || i == 45) {
+        continue;
+      }
+      int num = plateStack.pop();
+      assertEquals(num, i);
+    }
+  }
 }

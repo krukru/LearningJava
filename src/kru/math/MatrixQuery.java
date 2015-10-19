5f8471e1904fbee82eba706193951199783ec53b
Kru <kruljac.marko@gmail.com>
2015-10-19 16:53:41 +0200

    added todo

---
+++ src/kru/math/MatrixQuery.java
@@ -0,0 +1,10 @@
+package kru.math;
+
+import java.awt.*;
+
+public class MatrixQuery {
+
+  public int sum(Point from, Point to) {
+    return 0;
+  }
+}
---
+++ src/kru/misc/ExcelHelper.java
@@ -0,0 +1,4 @@
+package kru.misc;
+
+public class ExcelHelper {
+}

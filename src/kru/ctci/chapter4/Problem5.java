edf93d98b96e04d536c8feac3e719acef8c7695f
Kru <kruljac.marko@gmail.com>
2015-10-20 01:04:45 +0200

    fixed bug

--- src/kru/ctci/chapter4/Problem5.java
+++ src/kru/ctci/chapter4/Problem5.java
@@ -6,54 +6,23 @@ import kru.trees.binarytree.Node;
 public class Problem5 {
 
   public <E extends Comparable<E>> boolean isBinarySearchTree(BinaryTree<E> tree) {
-    return isLeftSubTreeBST(tree.root.leftChild, tree.root.data, tree.root.data) &&
-        isRightSubTreeBST(tree.root.rightChild, tree.root.data, tree.root.data);
+    return isSubTreeBST(tree.root.leftChild, null, tree.root.data) &&
+        isSubTreeBST(tree.root.rightChild, tree.root.data, null);
   }
 
-  private <E extends Comparable<E>> boolean isLeftSubTreeBST(Node<E> node, E minValue, E maxValue) {
+  private <E extends Comparable<E>> boolean isSubTreeBST(Node<E> node, E minValue, E maxValue) {
     if (node == null) {
       return true;
     }
-    if (node.data.compareTo(minValue) > 0 || node.data.compareTo(maxValue) < 0) {
+    if (minValue != null && node.data.compareTo(minValue) < 0 || maxValue != null && node.data
+        .compareTo(maxValue) > 0) {
       return false;
     }
+
     boolean leftChildOk;
-    leftChildOk = node.leftChild == null || isLeftSubTreeBST(node.leftChild, min(minValue, node
-        .data), max(maxValue, node.data));
-    boolean result = leftChildOk && (node.rightChild == null || isRightSubTreeBST(node
-        .rightChild, node.data, max(maxValue, node.data)));
+    leftChildOk = node.leftChild == null || isSubTreeBST(node.leftChild, minValue, node.data);
+    boolean result = leftChildOk && (node.rightChild == null || isSubTreeBST(node.rightChild,
+        node.data, maxValue));
     return result;
-  }
-
-  private <E extends Comparable<E>> boolean isRightSubTreeBST(Node<E> node, E minValue, E
-      maxValue) {
-    if (node == null) {
-      return true;
-    }
-    if (node.data.compareTo(maxValue) < 0 || node.data.compareTo(minValue) > 0) {
-      return false;
-    }
-    boolean leftChildOk;
-    leftChildOk = node.leftChild == null || isLeftSubTreeBST(node.leftChild, min(minValue, node
-        .data), node.data);
-    boolean result = leftChildOk && (node.rightChild == null || isRightSubTreeBST(node
-        .rightChild, min(minValue, node.data), max(maxValue, node.data)));
-    return result;
-  }
-
-  private <E extends Comparable<E>> E min(E item1, E item2) {
-    if (item1.compareTo(item2) <= 0) {
-      return item1;
-    } else {
-      return item2;
-    }
-  }
-
-  private <E extends Comparable<E>> E max(E item1, E item2) {
-    if (item1.compareTo(item2) >= 0) {
-      return item1;
-    } else {
-      return item2;
-    }
   }
 }

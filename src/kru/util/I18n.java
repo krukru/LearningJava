02521a1665e0f418552a33bb05861bfcfe1b1a37
Kru <kruljac.marko@gmail.com>
2015-10-19 15:12:15 +0200

    finished

--- src/kru/util/I18n.java
+++ src/kru/util/I18n.java
@@ -1,5 +1,9 @@
 package kru.util;
 
+import kru.collections.HashSet;
+
+import java.util.Collection;
+
 public class I18n {
 
   public String shorten(String input) {
@@ -10,5 +14,20 @@ public class I18n {
     Character firstCharacter = input.charAt(0);
     Character lastCharacter = input.charAt(input.length() - 1);
     return String.format("%1$s%2$s%3$s", firstCharacter, i18Length, lastCharacter);
+  }
+
+  public boolean doesCollectionUniquelyAbbreviate(Collection<String> stringsToShorten) {
+    boolean result = true; //we start by assumend it's true and work our way trying to disprove it.
+    HashSet<String> lookup = new HashSet<>();
+    for (String input : stringsToShorten) {
+      String i18nAbbreviation = shorten(input);
+      if (lookup.contains(i18nAbbreviation)) {
+        result = false;
+        break;
+      } else {
+        lookup.add(i18nAbbreviation);
+      }
+    }
+    return result;
   }
 }
--- test/kru/util/I18nTest.java
+++ test/kru/util/I18nTest.java
@@ -1,5 +1,6 @@
 package kru.util;
 
+import kru.collections.LinkedList;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,23 @@ public class I18nTest {
   public void testShorten() throws Exception {
     String s1 = "Internationalization";
     assertEquals("I18n", i18n.shorten(s1));
+  }
+
+  @Test
+  public void testAreAbbreviationsUnique_PositiveTest() throws Exception {
+    LinkedList<String> strings = new LinkedList<>();
+    strings.addLast("aaa");
+    strings.addLast("aab");
+    strings.addLast("aac");
+    assertTrue(i18n.doesCollectionUniquelyAbbreviate(strings));
+  }
+
+  @Test
+  public void testAreAbbreviationsUnique_NegativeTest() throws Exception {
+    LinkedList<String> strings = new LinkedList<>();
+    strings.addLast("aaa");
+    strings.addLast("aab");
+    strings.addLast("aaa");
+    assertFalse(i18n.doesCollectionUniquelyAbbreviate(strings));
   }
 }
